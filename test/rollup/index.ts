6c0ee4a60edc25181377da12f4c573f485a98951
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-02-09 13:58:04 +0800

    test: fix

--- test/rollup/index.ts
+++ test/rollup/index.ts
@@ -1,10 +1,11 @@
+import path from 'path';
 import { rollup } from 'rollup';
 import { rollup as rollupPlugin } from 'unplugin-confetti';
 
 test('rollup', () => {
     expect(() => {
         rollup({
-            input: './fixture/index.js',
+            input: path.resolve(__dirname, './fixture/index.js'),
             plugins: [rollupPlugin()],
         });
     }).not.toThrow();
